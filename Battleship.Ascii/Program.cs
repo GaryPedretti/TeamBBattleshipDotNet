e273f1b8fbd139497109f158aae986af486625db
TeamB3-GP <102955114+TeamB3-GP@users.noreply.github.com>
2025-04-23 17:13:54 +0000

    Game does not end Fixes #3

--- Battleship.Ascii/Program.cs
+++ Battleship.Ascii/Program.cs
@@ -81,6 +81,23 @@ namespace Battleship.Ascii
                 telemetryClient.TrackEvent("Player_ShootPosition", new Dictionary<string, string>() { { "Position", position.ToString() }, { "IsHit", isHit.ToString() } });
                 if (isHit)
                 {
+                    GameController.RecordHit(position);
+                    if (!enemyFleet.Any())
+                    {
+                        Console.WriteLine("You are the winner!");
+                        telemetryClient.TrackEvent("Game_Over", new Dictionary<string, string>() { { "Result", "Won" } });
+                        Environment.Exit(0);
+                    }
+                    foreach (var ship in enemyFleet.ToList())
+                    {
+                        var sunkShip = GameController.IsShipSunk(ship);
+                        if (sunkShip == true)
+                        {
+                            enemyFleet.Remove(ship);
+                            Console.WriteLine("Oh no! The computer sunk your {0}!", ship.Name);
+                            telemetryClient.TrackEvent("Computer_SunkShip", new Dictionary<string, string>() { { "Ship", ship.Name } });
+                        }
+                    }
                     Console.Beep();
 
                     Console.WriteLine(@"                \         .  ./");
@@ -102,6 +119,23 @@ namespace Battleship.Ascii
                 Console.WriteLine("Computer shot in {0}{1} and {2}", position.Column, position.Row, isHit ? "has hit your ship !" : "missed");
                 if (isHit)
                 {
+                    GameController.RecordHit(position);
+                    if (!myFleet.Any())
+                    {
+                        Console.WriteLine("You lost!");
+                        telemetryClient.TrackEvent("Game_Over", new Dictionary<string, string>() { { "Result", "Lost" } });
+                        Environment.Exit(0);
+                    }
+                    foreach (var ship in myFleet.ToList())
+                    {
+                        var sunkShip = GameController.IsShipSunk(ship);
+                        if (sunkShip == true)
+                        {
+                            myFleet.Remove(ship);
+                            Console.WriteLine("Oh no! The computer sunk your {0}!", ship.Name);
+                            telemetryClient.TrackEvent("Computer_SunkShip", new Dictionary<string, string>() { { "Ship", ship.Name } });
+                        }
+                    }
                     Console.Beep();
 
                     Console.WriteLine(@"                \         .  ./");
